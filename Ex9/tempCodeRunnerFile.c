5b169898054249c1099ee722b57cf780cb6dbf00
dangminhkhoi2212 <kkhoi600@gmail.com>
2022-09-20 10:15:31 +0700

    update KenKen

---
+++ Ex9/tempCodeRunnerFile.c
@@ -0,0 +1,9 @@
+for(i=0; i<kenken.NBlock; i++){
+		int sizeList=kenken.blocks[i].listCoord.size;
+		for(j=0; j<sizeList; j++){
+			Coord pos=kenken.blocks[i].listCoord.data[j];
+			if(row!=pos.x || column!=pos.y)
+				if(addConstrain(constrains, position, pos))
+					appendListCoord(changeds, pos);
+		}
+	}
